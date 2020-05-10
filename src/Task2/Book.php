bc58d709f6fbeff19f28fac4817edf86398c34f9
Vitalii Vitkovskyi <filosoff13@gmail.com>
2020-05-10 14:25:25 +0300

    Complite task#2

--- src/Task2/Book.php
+++ src/Task2/Book.php
@@ -6,18 +6,33 @@ namespace App\Task2;
 
 class Book
 {
+    protected string $title;
+    protected int $price;
+    protected int $pages;
+
+    public function __construct(
+        string $title,
+        int $price,
+        int $pages
+    )
+    {
+        $this->title = $title;
+        $this->price = $price;
+        $this->pages = $pages;
+    }
+
     public function getTitle(): string
     {
-        // @todo
+        return $this->title;
     }
 
     public function getPrice(): int
     {
-        // @todo
+        return $this->price;
     }
 
     public function getPagesNumber(): int
     {
-        //@todo
+        return $this->pages;
     }
 }
--- src/Task2/BooksGenerator.php
+++ src/Task2/BooksGenerator.php
@@ -4,10 +4,49 @@ declare(strict_types=1);
 
 namespace App\Task2;
 
+/**
+ * Class BooksGenerator
+ * @package App\Task2
+ */
 class BooksGenerator
 {
+    protected array $libraryBooks = [];
+    protected array $storeBooks = [];
+    protected int $maxPrice;
+    protected int $minPagesNumber;
+
+    /**
+     * BooksGenerator constructor.
+     * @param int $minPagesNumber
+     * @param array $libraryBooks
+     * @param int $maxPrice
+     * @param array $storeBooks
+     */
+    public function __construct(
+        int $minPagesNumber,
+        array $libraryBooks,
+        int $maxPrice,
+        array $storeBooks
+    )
+    {
+        $this->libraryBooks = $libraryBooks;
+        $this->storeBooks = $storeBooks;
+        $this->maxPrice = $maxPrice;
+        $this->minPagesNumber = $minPagesNumber;
+    }
+
     public function generate(): \Generator
     {
-        //@todo
+        foreach ($this->libraryBooks as $libraryBook){
+            if ($libraryBook->getPagesNumber() >= $this->minPagesNumber){
+                yield $libraryBook;
+            }
+        }
+
+        foreach ($this->storeBooks as $storeBook){
+            if ($storeBook->getPrice() < $this->maxPrice){
+                yield $storeBook;
+            }
+        }
     }
 }
