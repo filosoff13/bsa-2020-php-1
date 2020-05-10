96a9f49ff1b1d6cc9d15604090390a6534ba1d28
Vitalii Vitkovskyi <filosoff13@gmail.com>
2020-05-10 15:10:39 +0300

    Complite task#3

--- src/Task3/CarTrackHtmlPresenter.php
+++ src/Task3/CarTrackHtmlPresenter.php
@@ -11,6 +11,31 @@ class CarTrackHtmlPresenter
 {
     public function present(Track $track): string
     {
-        //@todo
+        $cars = $track->all();
+        $contentBlock = "<div align='center'><h1>Cars on Track</h1>";
+
+        foreach ($cars as $car) {
+            $carBlock = '<div style="background-color:lightblue">';
+
+            $carBlock .= "<p>Car number ". $car->getId() . "</p>";
+
+            $carBlock .= '<img src = "'. $car->getImage() .'" alt = "car image" height = "450">';
+
+            $carBlock .= "<p><b>Name: </b>". $car->getName() . "; " .
+                "<b>Max speed: </b>" . $car->getSpeed() . " km/h. </p>";
+            $carBlock .= "<p><b>Fuel tank volume: </b>" . $car->getFuelTankVolume()
+                . " litres</p>" . PHP_EOL;
+
+            $carBlock .= "<p>Fuel Consumption: " .
+                $car->getFuelConsumption() . " litres per 100 km "
+                . "</p>" . PHP_EOL;
+
+            $carBlock .= "</div>" . PHP_EOL . PHP_EOL;
+
+            $contentBlock .= $carBlock;
+        }
+        $contentBlock .= "</div>";
+
+        return $contentBlock;
     }
 }
